61352f78be7264cffaba411c5a9150c63ad31f04
gabrielsmm <gabrielsmm251@gmail.com>
2024-08-02 10:06:57 -0300

    feat: Classe de conexão obtendo usuario e senha do arquivo properties

--- src/main/java/com/gabrielsmm/soapclient/services/CorreiosService.java
+++ src/main/java/com/gabrielsmm/soapclient/services/CorreiosService.java
@@ -4,17 +4,18 @@ import com.gabrielsmm.soapclient.exceptions.ConexaoException;
 import com.gabrielsmm.soapclient.exceptions.ValidacaoException;
 import com.gabrielsmm.soapclient.util.Conexao;
 import com.gabrielsmm.soapclient.util.UteisMetodos;
-import com.gabrielsmm.soapclient.wsdl.*;
+import com.gabrielsmm.soapclient.wsdl.AtendeCliente;
+import com.gabrielsmm.soapclient.wsdl.EnderecoERP;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.lang.Exception;
 
 @Service
 public class CorreiosService {
 
     private AtendeCliente atendeCliente;
 
-    public CorreiosService() {
+    @Autowired
+    public CorreiosService(Conexao conexao) {
         atendeCliente = Conexao.getServicocorreio();
     }
 
@@ -22,7 +23,7 @@ public class CorreiosService {
         if (!UteisMetodos.validarCep(cep)) throw new ValidacaoException("CEP inválido.");
 
         try {
-            return atendeCliente.consultaCEP(cep, "", "");
+            return atendeCliente.consultaCEP(cep, Conexao.getUSUARIO(), Conexao.getSENHA());
         } catch (Exception e) {
             throw new ConexaoException("Erro ao realizar a consulta pelo CEP. " + e.getMessage());
         }
--- src/main/java/com/gabrielsmm/soapclient/util/Conexao.java
+++ src/main/java/com/gabrielsmm/soapclient/util/Conexao.java
@@ -1,21 +1,40 @@
 package com.gabrielsmm.soapclient.util;
 
 import com.gabrielsmm.soapclient.wsdl.AtendeCliente;
+import jakarta.annotation.PostConstruct;
 import jakarta.xml.ws.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.xml.namespace.QName;
 import java.net.URI;
 import java.net.URL;
 
+@Component
 public class Conexao {
 
     private static final String WSDL = "https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl";
     private static final String TARGET = "http://cliente.bean.master.sigep.bsb.correios.com.br/";
     private static final String NAME = "AtendeClienteService";
 
-    private static final AtendeCliente servicoCorreio = conexao();
+    @Value("${correios.usuario}")
+    private String usuario;
 
-    private static AtendeCliente conexao() {
+    @Value("${correios.senha}")
+    private String senha;
+
+    private static String USUARIO;
+    private static String SENHA;
+    private static AtendeCliente servicoCorreio;
+
+    @PostConstruct
+    private void init() {
+        USUARIO = this.usuario;
+        SENHA = this.senha;
+        servicoCorreio = conexao();
+    }
+
+    private AtendeCliente conexao() {
         try {
             URL url = new URI(WSDL).parseServerAuthority().toURL();
             QName qname = new QName(TARGET, NAME);
@@ -29,6 +48,14 @@ public class Conexao {
 
     public static AtendeCliente getServicocorreio() {
         return servicoCorreio;
+    }
+
+    public static String getUSUARIO() {
+        return USUARIO;
+    }
+
+    public static String getSENHA() {
+        return SENHA;
     }
 
 }
